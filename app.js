53fdca93702a9eb37655d51870de4729c1b6c8b1
Faiz <faizamodo@gmail.com>
2013-02-13 20:29:45 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express"); // imports express
+var app = express();        // create a new instance of express
+
+
+function initServer() {
+}
+
+// Finally, initialize the server, then activate the server at port 8889
+initServer();
+app.listen(8889);
