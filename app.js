b5355fa7e3b3167363b5227474ab21e8ade72670
Faiz <faizamodo@gmail.com>
2013-02-13 20:34:43 -0500

    Added the get method that serves up static files

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express"); // imports express
 var app = express();        // create a new instance of express
 
+// This is for serving files in the static directory
+app.get("/static/:staticFilename", function (request, response) {
+    response.sendfile("static/" + request.params.staticFilename);
+});
+
 
 function initServer() {
 }
