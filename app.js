b5a8785a2ef119586d0668dc12fd7d34b580dcc8
relshenawy <r.elshenawy@gmail.com>
2013-02-17 16:36:08 -0500

    some outlining of the server-side code and description of the data model

--- app.js
+++ app.js
@@ -8,7 +8,40 @@ app.get("/static/:staticFilename", function (request, response) {
 
 
 function initServer() {
+    loadData();
 }
+
+function loadData() {
+    loadQuestions();
+    loadStudents();
+}
+
+//read file with questions data and populate questions data structure 
+function loadQuestions() {
+
+}
+
+//read file with students data and populate students data structure 
+function loadStudents() {
+
+}
+
+function answerQuestion() {
+    //update question data
+    //update student data
+}
+
+function showQuestion(questionId) {
+    //for each student, get question by  questionId from questions data structure
+    //and update student's question data to contain it
+}
+
+
+function showAnswer(questionId) {
+    //for each student, get answer of questionId from questions data structure
+    //and update student's question data with answer
+}
+
 
 // Finally, initialize the server, then activate the server at port 8889
 initServer();
