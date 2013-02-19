499c8e743858a0921944f22af8ebe634f144571c
OpenShift guest <conlan.crothers@gmail.com>
2013-02-19 15:03:09 -0500

    updated student.js

--- static/student.js
+++ static/student.js
@@ -1,51 +1,77 @@
-var thingy; //global data storage
+var student = {};
+var questions = {"live": 0 , "array": []};//global data storage
 
-function RefreshDOM(){     
+function refreshDOM(){     
+
+    var index = questions.live
+    if (student.questions[index] === questions.array[index].answer){
+        //display correct DOM effect
+    }
+    else{
+        //display wrong answer screen and answer justification
+    }
+    
+    //add a button click response to call choose function
+
 }
 
 //initial get method fill
 // default to current question
 // refresh question call checks index of current viewed question
 
-function answer(choice){
-    //count the number of questions answered
-    //student.questions += 1
-    //save the students response
-    //student.answer = question.choice
-    //set the question 'state' to result, changing structure of DOM
-    //question.state = result
-    // display the correct answer somehow
-    if (question.choice === question.correct){
-        //need a question or student object element that effects DOM response as correct or incorrect
-        //record the students correctness
-        //student.correct+=1
-        // display the correct answer somehow
-    }
-    else {
-        //change DOM effecting object ot wrong
-    }
+
+function choose(ChoiceID){
+    var index =  questions.live;
+    student.question[index] = ChoiceID; //saves the student response in same array position as question
 }
 
-//get question
-
-//post student id, and student choice
-
-// get wait for question response data 
-
-//get student id
-
-//called after selecting the enxt question button
-function nextQuestion(){
-    $.
-    //changes the current question   
+function questionResults(answers){
+    var i = questions.live;
+    questions.array[i].answer = answers.correctAnswer; //adds the answerID to client side object array
+    questions.array[i].results = answers.studentAnswers; //adds class results for display
 }
 
-function asdf(){
+function getCorrectAnswer(question){
     $.ajax({
-        type : "post",
-        data: {id: studentid, stuff: questionid,  response: resonse},
-        url : "/asdf",
+        type: "get",
+        url: "/question/"+question.id,
         success: function(data){
+            questionResults(data);
+            refreshDOM();
+        }
+    })
+}
+
+function getStudentId(){
+    $.ajax({
+        type: "get",
+        url: "/studentid",
+        success: function(data){
+            student.id = data; //saves the student id in array
+            student.question = []; //starts an array for saving question responses
+        }
+    })
+}
+
+function nextQuestion(){
+    $.ajax({
+        type: "get",
+        url: "/question",
+        success: function(data){
+            questions.live = questions.array.length();
+            questions.array.push(data);
+            refreshDOM();
+        }
+    })
+}
+
+function sendAnswer(question, choice){
+    $.ajax({
+        type: "post",
+        url: "/question/"+question,
+        data: {"id" : student.id, "answer" : student.question[questions.live]},
+        success: function(data){
+            refreshDOM()
         }
     })
 }
