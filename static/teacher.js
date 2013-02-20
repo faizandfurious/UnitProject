f053f40828870224a167583e837f3715c095bf4c
Faiz <faizamodo@gmail.com>
2013-02-20 14:22:26 -0500

    Added ability to select fake quiz and see question/answer, and go back to fake quiz listing

--- static/teacher.js
+++ static/teacher.js
@@ -1,3 +1,12 @@
-$('.quiz').click(function(){
-	alert('clicked');
+$('.quiz_box').click(function(){
+	var ele = $('.quiz_box');
+	var par = ele.parent();
+	par.hide();
+	$('#quiz_display').css('visibility', 'visible');
 });
+
+$('#back_button').click(function(){
+	$('#quiz_display').css('visibility', 'hidden');
+	$('#quiz_listing').show();
+
+})
