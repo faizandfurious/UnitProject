f5acb99967af644173135de0228a377185375c7d
OpenShift guest <conlan.crothers@gmail.com>
2013-02-27 21:19:59 -0500

    got canvas to work

--- static/teacher.js
+++ static/teacher.js
@@ -429,6 +429,8 @@ function max(list){
 }
 
 function drawShell(){
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(0,0,500,400);
     ctx.fillStyle = "black";
     ctx.fillRect(50, 20, 2, 300);
     ctx.fillRect(50, 320, 400, 2);
@@ -587,5 +589,6 @@ function drawResults(students, questionID, questionChoices){
 $(document).ready(function() {
     $("#create_question_form").css('display', 'none');
     $("#page_title").html("Topics").css('visibility', 'visible');
+    drawShell();
     getQuestions();
 })
